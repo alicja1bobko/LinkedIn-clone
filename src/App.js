b639db8f25ddc10242fb276c45fc3908adfa4066
AlicjaBobko <alicja_bobko@o2.pl>
2022-10-19 16:57:22 +0200

    style:feed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import Feed from "./components/Feed/Feed";
 function App() {
   return (
     <div className="app">
       <Header />
       <div className="app__body">
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
