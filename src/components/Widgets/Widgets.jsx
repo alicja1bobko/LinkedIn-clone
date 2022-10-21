e10f0b56c3aceaf4e49273800827bb217ca82644
AlicjaBobko <alicja_bobko@o2.pl>
2022-10-21 13:13:36 +0200

    style: widgets

--- src/components/Widgets/Widgets.jsx
+++ src/components/Widgets/Widgets.jsx
@@ -1,13 +1,33 @@
 import "./Widgets.css";
 import InfoIcon from "@mui/icons-material/Info";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 
 const Widgets = () => {
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecordIcon />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
   return (
     <div className="widgets">
       <div className="widgets__header">
         <h2>LinkedIn News</h2>
         <InfoIcon />
       </div>
+      {newsArticle("Covid is gone", "Top news - 9099 readers")}
+      {newsArticle("Covid is gone", "Top news - 9099 readers")}
+      {newsArticle("Covid is gone", "Top news - 9099 readers")}
+      {newsArticle("Covid is gone", "Top news - 9099 readers")}
+      {newsArticle("Covid is gone", "Top news - 9099 readers")}
+      {newsArticle("Covid is gone", "Top news - 9099 readers")}
+      {newsArticle("Covid is gone", "Top news - 9099 readers")}
     </div>
   );
 };
